4d45417bc5f9246d18c4ce5f3e37575285bae08a
rocketpc <Tju_pc@163.com>
2019-07-25 22:50:26 +0800

    反转单链表-新增LeetCode206题遍历解法

--- src/linkedList/ReverseSingleLinkedList.java
+++ src/linkedList/ReverseSingleLinkedList.java
@@ -25,7 +25,7 @@ public class ReverseSingleLinkedList {
             h = h.getNext();
         }
         // 调用反转方法
-        head = Reverse1(head);
+        head = reverse3(head);
 //            head = reverse2(head);
 
         System.out.println("\n**************************");
@@ -36,84 +36,97 @@ public class ReverseSingleLinkedList {
         }
     }
 
-        /**
-         * 遍历，将当前节点的下一个节点缓存后更改当前节点指针
-         */
-        public static Node reverse2 (Node head){
-            if (head == null) {
-                return null;
-            }
-            // 上一结点
-            Node pre = head;
-            // 当前结点
-            Node cur = head.getNext();
-            // 临时结点，用于保存当前结点的指针域（即下一结点）
-            Node tmp;
-            // 当前结点为null，说明位于尾结点
-            while (cur != null) {
-                tmp = cur.getNext();
-                // 反转指针域的指向
-                cur.setNext(pre);
+    /**
+     * 遍历实现1
+     * @param head 待遍历的链表
+     * @return  反转后的链表
+     */
+    public static Node reverse3(Node head) {
+        Node curr = head;
+        Node pre = null;
+        while (curr != null) {
+            Node next = curr.getNext();
+            curr.Next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
 
-                // 指针往下移动
-                pre = cur;
-                cur = tmp;
-            }
-            // 最后将原链表的头节点的指针域置为null，还回新链表的头结点，即原链表的尾结点
+    /**
+     * 遍历，将当前节点的下一个节点缓存后更改当前节点指针
+     */
+    public static Node reverse2 (Node head){
+        if (head == null) {
+            return null;
+        }
+        // 上一结点
+        Node pre = head;
+        // 当前结点
+        Node cur = head.getNext();
+        // 临时结点，用于保存当前结点的指针域（即下一结点）
+        Node tmp;
+        // 当前结点为null，说明位于尾结点
+        while (cur != null) {
+            tmp = cur.getNext();
+            // 反转指针域的指向
+            cur.setNext(pre);
+
+            // 指针往下移动
+            pre = cur;
+            cur = tmp;
+        }
+        // 最后将原链表的头节点的指针域置为null，还回新链表的头结点，即原链表的尾结点
+        head.setNext(null);
+        return pre;
+    }
+
+    /**
+     * 递归，在反转当前节点之前先反转后续节点
+     */
+    private static Node Reverse1(Node head) {
+        // head看作是前一结点，head.getNext()是当前结点，reHead是反转后新链表的头结点
+        if (head == null || head.getNext() == null) {
+            // 若为空链或者当前结点在尾结点，则直接还回
+            return head;
+        } else {
+            // 先反转后续节点head.getNext()
+            Node reHead = Reverse1(head.getNext());
+            // 将当前结点的指针域指向前一结点
+            head.getNext().setNext(head);
+            // 前一结点的指针域令为null;
             head.setNext(null);
-            return pre;
-        }
-
-        /**
-         * 递归，在反转当前节点之前先反转后续节点
-         */
-        private static Node Reverse1(Node head) {
-            // head看作是前一结点，head.getNext()是当前结点，reHead是反转后新链表的头结点
-            if (head == null || head.getNext() == null) {
-                // 若为空链或者当前结点在尾结点，则直接还回
-                return head;
-            } else {
-                // 先反转后续节点head.getNext()
-                Node reHead = Reverse1(head.getNext());
-                // 将当前结点的指针域指向前一结点
-                head.getNext().setNext(head);
-                // 前一结点的指针域令为null;
-                head.setNext(null);
-                // 反转后新链表的头结点
-                return reHead;
-            }
+            // 反转后新链表的头结点
+            return reHead;
         }
     }
+}
 
-    class Node {
-        /**
-         * 数据域
-         */
-        private int Data;
-        /**
-         * 指针域
-         */
-        private Node Next;
+class Node {
+    /**
+     * 数据域
+     */
+    int Data;
+    /**
+     * 指针域
+     */
+    Node Next;
 
-        public Node(int Data) {
-            // super();
-            this.Data = Data;
-        }
-
-        public int getData() {
-            return Data;
-        }
-
-        public void setData(int Data) {
-            this.Data = Data;
-        }
-
-        public Node getNext() {
-            return Next;
-        }
-
-        public void setNext(Node Next) {
-            this.Next = Next;
-        }
+    public Node(int Data) {
+        // super();
+        this.Data = Data;
     }
+
+    public int getData() {
+        return Data;
+    }
+
+    public Node getNext() {
+        return Next;
+    }
+
+    public void setNext(Node Next) {
+        this.Next = Next;
+    }
+}
 
